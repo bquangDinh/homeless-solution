f8ee33cc5033ad10973f08ee39cd5fdbd2e46cf3
Quang Bui <quangbui@nbp-29-84.nbp.ruw.rutgers.edu>
2023-10-08 01:40:38 -0400

    FInal

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,7 +1,7 @@
 import './style.css'
 export default function ExplorePage(){
 	return <>
-	
+
 	Explore page
 
 	<div className='center'>
@@ -9,24 +9,38 @@ export default function ExplorePage(){
 		<p>Food Bank available: 2 Chicken rolls</p>
 		<div className='Task'>
 		<div className = 'rectangle'>
-			<button type="button">Clear the yard for us</button>
+			<button type="button">
+				<a href='/explore/1'>Clear the yard for us</a>
+			</button>
 		</div>
 		<br></br>
 		<div className = 'rectangle' >
-			<button type="button">Take out the trash for us</button>
+			<button type="button">
+				<a href='/explore/1'>
+					Take out the trash for us
+				</a>
+			</button>
 		</div>
 		</div>
 	</div>
 	<div className='center'>
-	<p>McDonald's Store #2345</p>
+	<p>{"McDonald's Store #2345"}</p>
 		<p>Food Bank available: 1 Burger reward</p>
 		<div className='Task'>
 		<div className = 'rectangle'>
-			<button type="button">Clear the yard for us</button>
+			<button type="button">
+				<a href='/explore/2'>
+				Clear the yard for us
+				</a>
+			</button>
 		</div>
 		<br></br>
 		<div className = 'rectangle' >
-			<button type="button">Take out the trash for us</button>
+			<button type="button">
+			<a href='/explore/2'>
+					Take out the trash for us
+				</a>
+			</button>
 		</div>
 		</div>
 	</div>
---
+++ src/app/store/auth/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Card, Typography } from 'antd'
+import './style.css'
+
+export default function StoreAuthPage() {
+	return <div className='w-screen h-screen flex flex-col justify-center items-center'>
+		<div className='auth-card flex flex-col justify-center items-center'>
+			<Typography.Title level={3}>New member?</Typography.Title>
+
+		</div>
+	</div>
+}
